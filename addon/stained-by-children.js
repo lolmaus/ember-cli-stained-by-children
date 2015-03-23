03f58ce242bc7452a9da7bf57326b56b5742b575
James Murphy <jamesmurphyau@me.com>
2015-03-23 12:49:13 +1100

    Supports isDirty with deprecation

--- addon/stained-by-children.js
+++ addon/stained-by-children.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
+
   isFilthy: Ember.computed('areChildrenDirty', 'currentState.isDirty', function() {
     return this.get('currentState.isDirty') || this.get('areChildrenDirty');
   }),
@@ -17,8 +18,16 @@ export default Ember.Mixin.create({
     // Setting the `areChildrenDirty` property dynamically.
     // This is required in order to bind it to all relationships.
     this._defineAreChildrenDirty();
+    this._deprecatedDefineIsDirty();
   },
 
+  _deprecatedDefineIsDirty: function() {
+    Ember.defineProperty(this, 'isDirty', Ember.computed('areChildrenDirty', 'currentState.isDirty', function() {
+      return this.get('currentState.isDirty') || this.get('areChildrenDirty');
+    }));
+
+    Ember.deprecate('ember-cli-stained-by-children: redifining the \'isDirty\' property is no longer allowed in Ember and will be removed in a future version of ember-cli-stained-by-children. Please use \'isFilthy\' instead.', false);
+  },
 
   _defineAreChildrenDirty: function() {
     var
@@ -60,9 +69,9 @@ export default Ember.Mixin.create({
   _propertyNameForRelationship: function(relationshipName, relationship) {
     var propertyName = relationshipName;
     if (relationship.kind === 'hasMany') {
-      propertyName += ".@each.isDirty";
+      propertyName += ".@each.{isDirty,isFilthy}";
     } else {
-      propertyName += ".isDirty";
+      propertyName += ".{isDirty,isFilthy}";
     }
     return propertyName;
   },
@@ -108,11 +117,11 @@ export default Ember.Mixin.create({
 
     if (relationship.kind === 'hasMany') {
       return child.any(function (item) {
-        return item.get('isDirty');
+        return item.get('isFilthy') || item.get('isDirty');
       });
 
     } else {
-      return child.get('isDirty');
+      return child.get('isFilthy') || child.get('isDirty');
     }
   }
 });
